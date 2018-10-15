62a71e469f307efb8fc14ad8ca0dd10c5d94c22f
Vu Tran <vutran6853@gmail.com>
2018-10-15 17:01:36 -0500

    Jordan/Vu updates to redux and map

--- src/components/googleMapTest/MapContainer.js
+++ src/components/googleMapTest/MapContainer.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import AutoCompleteMap from './AutoCompleteMap';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import AutoCompleteMap from "./AutoCompleteMap";
+import { getHospitals } from "../../dux/reducer";
 
-const GOOGLE_MAP_KEY = process.env.REACT_APP_GOOGLE_MAP_KEY
+const GOOGLE_MAP_KEY = process.env.REACT_APP_GOOGLE_MAP_KEY;
 
 class MapContainer extends Component {
-  constructor(props){
-    super(props)
-      this.state = { 
-        showingInfoWindow: false,
-        activeMarker: {},
-        selectedPlace: {},
-       }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
     this.handleOnMarkerClick = this.handleOnMarkerClick.bind(this);
   }
 
+  componentDidMount() {
+    this.props.getHospitals();
+    this.gimmeName();
+  }
+
   handleOnMarkerClick(props, marker, e) {
-    console.log('props', props)
-    console.log('marker', marker)
-    console.log('e', e)
+    // console.log("props", props);
+    // console.log("marker", marker);
+    // console.log("e", e);
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
@@ -28,53 +35,78 @@ class MapContainer extends Component {
 
   handleOnMapClicked(props) {
     // console.log('props', props)
-    if(this.state.showingInfoWindow) {
-      console.log('this.state.showingInfoWindow', this.state.showingInfoWindow) 
-      this.setState({ showingInfoWindow: false, activeMarker: null })
+    if (this.state.showingInfoWindow) {
+      console.log("this.state.showingInfoWindow", this.state.showingInfoWindow);
+      this.setState({ showingInfoWindow: false, activeMarker: null });
     }
   }
 
-  render() { 
+  gimmeName = () => {
+    setTimeout(() => {
+      if (this.props.hospitals[0]) {
+        return this.setState({
+          hospital_name: this.props.hospitals[0].hospital_name,
+          hospital_address: this.props.hospitals[0].hospital_address
+        });
+        // return this.props.hospitals["'0'"].hospital_name;
+      } else {
+        this.setState({ hospital_name: "Hospital Name here" });
+      }
+      // console.log("hospital1", hospital1[0].hospital_name);
+    }, 2000);
+  };
+
+  render() {
     const style = {
-      width: '50%',
-      height: '50%'
-    }
-    
-    return ( 
+      width: "50%",
+      height: "50%"
+    };
+
+    return (
       <div>
         <p>Map 1 this will show location and marker tootip hospital info </p>
-        
-        <Map google={ this.props.google } 
-             onClick={ () => this.handleOnMapClicked() }
-             zoom={ 14 }
-             style={style}
-             initialCenter={ { lat: 32.7773293, lng: -96.7963455 } }   // <- INTI START POINT OF MAP LOCATION VIEW
-        >   
+
+        <Map
+          google={this.props.google}
+          onClick={() => this.handleOnMapClicked()}
+          zoom={14}
+          style={style}
+          initialCenter={{ lat: 32.7773293, lng: -96.7963455 }} // <- INTI START POINT OF MAP LOCATION VIEW
+        >
           <Marker
-            onClick={ this.handleOnMarkerClick }
-            title={'The marker`s title will appear as a tooltip.'}
-            name={'Dallas Area1'}
-            position={ { lat: 32.7773293, lng: -96.7963455 } } />
-            
+            onClick={this.handleOnMarkerClick}
+            title={"The marker`s title will appear as a tooltip."}
+            name={this.state.hospital_name}
+            position={{ lat: 32.7773293, lng: -96.7963455 }}
+          />
+
           <Marker
-            onClick={ this.handleOnMarkerClick }
-            title={'The marker`s title will appear as a tooltip.'}
-            name={'Dallas Area2'}
-            position={ { lat: 32.5773293, lng: -96.7963455 } } />
-          
+            onClick={this.handleOnMarkerClick}
+            title={"The marker`s title will appear as a tooltip."}
+            name={"Dallas Area2"}
+            position={{ lat: 32.5773293, lng: -96.7963455 }}
+          />
+
           <InfoWindow
             marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
           </InfoWindow>
-          
-          <AutoCompleteMap/>
+
+          <AutoCompleteMap />
         </Map>
       </div>
-     );
+    );
   }
 }
- 
-export default GoogleApiWrapper( { apiKey: `${GOOGLE_MAP_KEY}` } )(MapContainer);
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+export default connect(mapStateToProps, { getHospitals })(
+  GoogleApiWrapper({ apiKey: `${GOOGLE_MAP_KEY}` })(MapContainer)
+);
--- src/dux/reducer.js
+++ src/dux/reducer.js
@@ -1,133 +1,143 @@
-import axios from 'axios';
+import axios from "axios";
 
 const initialState = {
-    profile: [],
-    files: [],
-    hospitals: [],
-    matched: [],
-    isLoading: false
-}
+  profile: [],
+  files: [],
+  hospitals: [],
+  matched: [],
+  isLoading: false
+};
 
-const GET_PROFILES = 'GET_PROFILES'
-const GET_FILES = 'GET_FILES'
-const GET_HOSPITALS = 'GET_HOSPITALS'
-const GET_MATCHED = 'GET_MATCHED'
+const GET_PROFILES = "GET_PROFILES";
+const GET_FILES = "GET_FILES";
+const GET_HOSPITALS = "GET_HOSPITALS";
+const GET_MATCHED = "GET_MATCHED";
 
 export const getProfiles = () => {
-    return {
-        type: GET_PROFILES,
-        payload: axios.get('/api/profile')
-        .then(response => {return response.data})
-        .catch(err => console.log(err))
-    }
-}
+  return {
+    type: GET_PROFILES,
+    payload: axios
+      .get("/api/profile")
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => console.log(err))
+  };
+};
 
 export const getFiles = () => {
-    return {
-        type: GET_FILES,
-        payload: axios.get('/api/files')
-        .then(response => {return response.data})
-        .catch( err=> console.log(err))
-    }
-}
+  return {
+    type: GET_FILES,
+    payload: axios
+      .get("/api/files")
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => console.log(err))
+  };
+};
 
 export const getHospitals = () => {
-    return {
-        type: GET_HOSPITALS,
-        payload: axios.get('/api/hospitals')
-        .then(response => {return response.data})
-        .catch(err=> console.log(err))
-    }
-}
+  return {
+    type: GET_HOSPITALS,
+    payload: axios
+      .get("/api/hospitals")
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => console.log(err))
+  };
+};
 
 export const getMatched = () => {
-    return {
-        type: GET_MATCHED,
-        payload: axios.get('/api/hospitals')
-        .then(response => {return response.data})
-        .catch(err => console.log(err))
-    }
-}
-
-
+  return {
+    type: GET_MATCHED,
+    payload: axios
+      .get("/api/hospitals")
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => console.log(err))
+  };
+};
 
 export default function reducer(state = initialState, action) {
-    switch(action.type){
-        case `${GET_PROFILES}_PENDING`:
-        return {
-            ...state,
-            isLoading: true
-        }
+  switch (action.type) {
+    case `${GET_PROFILES}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
 
-        case `${GET_PROFILES}_FULFILLED`:
-        return {
-            ...state,
-            profile: action.payload,
-            isLoading: false
-        }
+    case `${GET_PROFILES}_FULFILLED`:
+      return {
+        ...state,
+        profile: action.payload,
+        isLoading: false
+      };
 
-        case `${GET_PROFILES}_REJECTED`:
-        return {
-            ...state,
-            isLoading: false
-        }
+    case `${GET_PROFILES}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false
+      };
 
-        case `${GET_FILES}_PENDING`:
-        return {
-            ...state,
-            isLoading: true
-        }
+    case `${GET_FILES}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
 
-        case `${GET_FILES}_FULFILLED`:
-        return {
-            ...state,
-            files: action.payload,
-            isLoading: false
-        }
+    case `${GET_FILES}_FULFILLED`:
+      return {
+        ...state,
+        files: action.payload,
+        isLoading: false
+      };
 
-        case `${GET_FILES}_REJECTED`:
-        return {
-            ...state,
-            isLoading: false
-        }
+    case `${GET_FILES}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false
+      };
 
-        case `${GET_HOSPITALS}_PENDING`:
-        return {
-            ...state,
-            isLoading: true
-        }
+    case `${GET_HOSPITALS}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
 
-        case `${GET_HOSPITALS}_FULFILLED`:
-        return {
-            ...state,
-            hospitals: action.payload,
-            isLoading: false
-        }
+    case `${GET_HOSPITALS}_FULFILLED`:
+      return {
+        ...state,
+        hospitals: action.payload
+      };
 
-        case `${GET_HOSPITALS}_REJECTED`:
-        return {
-            ...state,
-            isLoading: false
-        }
+    case `${GET_HOSPITALS}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false
+      };
 
-        case `${GET_MATCHED}_PENDING`:
-        return {
-            ...state,
-            isLoading: true
-        }
+    case `${GET_MATCHED}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
 
-        case `${GET_MATCHED}_FULFILLED`:
-        return {
-            ...state,
-            matched: action.payload,
-            isLoading: false
-        }
+    case `${GET_MATCHED}_FULFILLED`:
+      return {
+        ...state,
+        matched: action.payload,
+        isLoading: false
+      };
 
-        case `${GET_MATCHED}_REJECTED`:
-        return {
-            ...state,
-            isLoading: false
-        }
-    default: return {...state};
-    }
+    case `${GET_MATCHED}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false
+      };
+    default:
+      return { ...state };
+  }
 }
