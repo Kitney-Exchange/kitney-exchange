4257f2a04d1c94a77a81669bb0e443bda7419426
Bonnie Jordan <thebonstergirl@gmail.com>
2018-10-15 15:10:21 -0500

    Jordan reducer finished

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import reducer from "./dux/reducer";
+import promiseMiddleware from 'redux-promise-middleware';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middlewares = composeEnhancers(applyMiddleware(promiseMiddleware()));
+
+const store = createStore(reducer, middlewares);
 
 export default store;
