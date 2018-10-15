4e16bf1b5e69c5a6d12a15cc5a8e77d022b63f51
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-10-15 09:16:48 -0500

    ashley routing

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import react from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <div className="banner">
+        <div className="banner-img" />
+        <div className="banner-text" />
+      </div>
+      <div className="aboutus">
+        <div className="aboutus-img" />
+        <div className="aboutus-text" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
