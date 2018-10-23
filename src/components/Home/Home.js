31abdece35e51afd8bcee9399db114e9ac186c3d
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-10-23 15:23:55 -0500

    styling ashley

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,24 +6,9 @@ import singleline from "../../images/singleline.png";
 import sun from "../../images/sun.png";
 import twohands from "../../images/twohands.jpeg";
 import { Parallax, Background } from "react-parallax";
-import { UncontrolledCarousel } from "reactstrap";
+import { Carousel } from "react-responsive-carousel";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Link } from "react-router-dom";
-
-// images for carousel
-const items = [
-  {
-    src:
-      "https://images.unsplash.com/photo-1531573241436-069768aa6b81?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d85acfd5e5a690e1b9fb9b9cb1ac114f&auto=format&fit=crop&w=1350&q=80"
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1536551739350-d473d0f5d66a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=008122b00ce6617ca065ffff90c78e11&auto=format&fit=crop&w=1350&q=80"
-  },
-  {
-    src:
-      "https://images.unsplash.com/photo-1498568715259-5c1dc96aa8e7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ad416811db9057a089db51b5b7f97794&auto=format&fit=crop&w=1350&q=80"
-  }
-];
 
 class Home extends Component {
   render() {
@@ -31,20 +16,15 @@ class Home extends Component {
       <div>
         <div className="homepage">
           <div className="banner-top">
-            {/* This will be parallax scrolling! :D 
-            it will also be a scrolling navbar as well, but only on home page */}
             <div className="nav-link">How It Works</div>
             <div className="nav-link">About</div>
             <div className="nav-link">Success Stories</div>
             <div className="nav-link">Sign Up</div>
-            {/* <div className="banner-title">Matching kidneys. Saving lives.</div> */}
-            {/* <div className="banner-login">Login</div> */}
           </div>
           <div className="banner">
             <div className="banner-img">
               <img
                 id="banner-img"
-                // src="http://ukhealthcare.net/wp-content/uploads/2018/04/living-kidney-donor.jpg"
                 src="https://images.unsplash.com/photo-1432838598623-de06c44610e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=492392ff81bd03d455b058c093bc3eba&auto=format&fit=crop&w=1500&q=80"
                 alt="banner img"
                 style={{
@@ -78,7 +58,6 @@ class Home extends Component {
                     }}
                   />
                 </div>
-                {/* <div className="signup-button">Create Account</div> */}
                 <Link id="link-dec" to="/register">
                   <div className="login-button">Register Now</div>
                 </Link>
@@ -145,7 +124,7 @@ class Home extends Component {
                   width: "60px",
                   height: "60px"
                 }}
-              />{" "}
+              />
             </div>
             <div className="success-text">
               <p id="aboutus-text">
@@ -157,21 +136,48 @@ class Home extends Component {
                 calf donkey duck.
               </p>
             </div>
-            {/* carousel imported from reactstrap, need to edit to manual slide, and fix position */}
             <div className="carousel">
-              <UncontrolledCarousel
-                keyboard={false}
-                pause={false}
-                autoplay={false}
-                ride="carousel"
-                interval="4000"
-                slide={true}
-                className="carousel-fade"
-                items={items}
-                controls={false}
-                indicators={false}
-              />
+              <Carousel
+                showArrows={true}
+                showThumbs={false}
+                infiniteLoop={true}
+                autoPlay={true}
+                id="carousel"
+              >
+                <div>
+                  <img src="https://images.unsplash.com/photo-1531573241436-069768aa6b81?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d85acfd5e5a690e1b9fb9b9cb1ac114f&auto=format&fit=crop&w=1350&q=80" />
+                  <p className="legend">
+                    Carrots soybeans, owls duck raising or, cheep in plows.
+                    Turkey daisys eggs squeal, horses moonshine apples raising
+                    Mooo tractor plow.
+                  </p>
+                </div>
+                <div>
+                  <img src="https://images.unsplash.com/photo-1536551739350-d473d0f5d66a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=008122b00ce6617ca065ffff90c78e11&auto=format&fit=crop&w=1350&q=80" />
+                  <p className="legend">
+                    Carrots soybeans, owls duck raising or, cheep in plows.
+                    Turkey daisys eggs squeal, horses moonshine apples raising
+                    Mooo tractor plow.
+                  </p>
+                </div>
+                <div>
+                  <img src="https://images.unsplash.com/photo-1498568715259-5c1dc96aa8e7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ad416811db9057a089db51b5b7f97794&auto=format&fit=crop&w=1350&q=80" />
+                  <p className="legend">
+                    Carrots soybeans, owls duck raising or, cheep in plows.
+                    Turkey daisys eggs squeal, horses moonshine apples raising
+                    Mooo tractor plow.
+                  </p>
+                </div>
+              </Carousel>
             </div>
+            <footer id="footer-home">
+              <ul>
+                <li>Kitney Exchange</li>
+                <li>Home</li>
+                <li>Contact Us</li>
+                <li>Admin</li>
+              </ul>
+            </footer>
           </div>
         </div>
       </div>
