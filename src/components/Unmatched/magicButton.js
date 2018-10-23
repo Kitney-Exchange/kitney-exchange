59935365340787bb88042231ec4a7304c2645e7b
Vu Tran <vutran6853@gmail.com>
2018-10-23 09:37:33 -0500

    VU update

--- src/components/Unmatched/magicButton.js
+++ src/components/Unmatched/magicButton.js
@@ -46,84 +46,96 @@ class MagicButton extends Component {
   // const possibleMatchPair = []
 
   for(let i = 0; i < array1.length; i++) {
-    console.log('');
+    // console.log('');
 
     for(let j = 0; j < array2.length; j++) {
       console.log(`donor2[${i}]`, array1[i].donor_blood_type, `recipient2[${j}]`, array2[j].recipient_blood_type)
-      console.log('Compare: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
+      // console.log('Compare: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
 
         if(array1[i].donor_blood_type === 'A' && array2[j].recipient_blood_type === 'A') {
-          console.log('Match a for a : ', array1[i].donor_blood_type, array2[j].recipient_blood_type )
-          console.log('Match a for a : ', array1[i], array2[j] )
+          // console.log('Match a for a : ', array1[i].donor_blood_type, array2[j].recipient_blood_type )
+          // console.log('Match a for a : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])
 
         } else if(array1[i].donor_blood_type === 'A' && array2[j].recipient_blood_type !== 'B' && array2[j].recipient_blood_type !== 'AB' && array2[j].recipient_blood_type !== 'O') {
-          console.log('Compare a for a: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
+          // console.log('Compare a for a: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
 
         } else if(array1[i].donor_blood_type === 'A' && array2[j].recipient_blood_type === 'AB' && array2[j].recipient_blood_type !== 'B') {
-          console.log('Match a for ab: ', array1[i], array2[j] )
+          // console.log('Match a for ab: ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         }
 
         if(array1[i].donor_blood_type === 'B' && array2[j].recipient_blood_type === 'B') {
-          console.log('Compare b for b : ', array1[i].pair_id, array2[j].pair_id )
-          console.log('Match b for b : ', array1[i], array2[j] )
+          // console.log('Compare b for b : ', array1[i].pair_id, array2[j].pair_id )
+          // console.log('Match b for b : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         } else if(array1[i].donor_blood_type === 'B' && array2[j].recipient_blood_type !== 'A' && array2[j].recipient_blood_type !== 'AB' && array2[j].recipient_blood_type !== 'O') {
-          console.log('Compare b for b: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
+          // console.log('Compare b for b: ',array1[i].donor_blood_type,  array2[j].recipient_blood_type )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         } else if(array1[i].donor_blood_type === 'B' && array2[j].recipient_blood_type === 'AB' && array2[j].recipient_blood_type !== 'A') {
-          console.log('Match b for ab : ', array1[i], array2[j] )
+          // console.log('Match b for ab : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         }
 
         if(array1[i].donor_blood_type === 'AB' && array2[j].recipient_blood_type === 'AB' || array1[i].donor_blood_type !== 'A' && array2[j].recipient_blood_type !== 'A' && array1[i].donor_blood_type !== 'B' && array2[j].recipient_blood_type !== 'B'  && array1[i].donor_blood_type !== 'O' && array2[j].recipient_blood_type !== 'O') {
-          console.log('Match ab for ab : ', array1[i], array2[j] )
+          // console.log('Match ab for ab : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         }
 
         if(array1[i].donor_blood_type === 'O' && array2[j].recipient_blood_type === 'O') {
-          console.log('Match o for o : ', array1[i], array2[j] )
+          // console.log('Match o for o : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
         } else if(array1[i].donor_blood_type === 'O' && array2[j].recipient_blood_type === 'A' || array1[i].donor_blood_type === 'O' && array2[j].recipient_blood_type === 'B' || array1[i].donor_blood_type === 'O' && array2[j].recipient_blood_type === 'AB') {
-          console.log('Match o for a, b, ab, o : ', array1[i], array2[j] )
+          // console.log('Match o for a, b, ab, o : ', array1[i], array2[j] )
           possibleMatchPair.push([array1[i].pair_id, array2[j].pair_id, array1[i], array2[j]])    
       }
     }
   }
 }
 
-finalBatcher = (arr1) => {
-  // console.log(arr1)
-  const initialArr= arr1.filter((e, i)=> e[i]);
+  finalBatcher = (array1) => {
+  console.log(array1)
+    
+
+
+
+  const initialArr= array1.filter((e, i)=> e[i]);
+    
   const idFinder = (a) => {
     const newArr = [];
-    for (let i = 0; i < initialArr.length; i++){
+
+    for (let i = 0; i < initialArr.length; i++){    
       newArr.push(initialArr[i][a])
     }
       return newArr;
   }
-  const DonorArr = idFinder(0);
+
+  const donorArr = idFinder(0);
   const RecipArr = idFinder(1);
   const finalResult = [];
-  console.log(DonorArr, RecipArr)
+  let testFinal = []
+
+  console.log(donorArr, RecipArr);
+  // console.log();
+  testFinal.push(donorArr.shift(), RecipArr.shift())
+  console.log('testFinal:', testFinal)
+  console.log(RecipArr.filter(testFinal[1]))
+  // finalResult.push(donorArr.shift(), RecipArr.shift())
+  // console.log(donorArr, RecipArr);
+  // console.log(donorArr.shift(), RecipArr.shift());
+  // console.log(donorArr, RecipArr);
+
+  console.log('finalResult:', finalResult[1])
   
-  finalResult.push([DonorArr[0], RecipArr[0]]);
+  // // finalResult.push([donorArr[0], RecipArr[0]]);
 
-  return finalResult;
+  
+  // return finalResult;
 
-  // for (let i = 0; i < DonorArr.length; i++){
-  //   // for (let j = 0; j < arr1.length; i++)
-  //   if (DonorArr[i] === RecipArr[i]){
-  //     finalResult.push([DonorArr[i], RecipArr[j]])
-  //   }
 
+    console.log('finalResult:', finalResult)
   }
-
-// findMatchPair(donor2, recipient2)
-// console.log('possibleMatchPair: ', possibleMatchPair)
-
 
   render() { 
     let profileList = this.props
